e0e42a5ccb79928286f528cc595b87eb1e1cbbf1
Shahill22 <shahill35.ms@gmail.com>
2022-12-12 16:44:58 +0300

    added ERC1594 for token transfer restrictions

--- contracts/tokens/ProjectToken.sol
+++ contracts/tokens/ProjectToken.sol
@@ -4,7 +4,6 @@ import "./ERC1594.sol";
 import "./ERC20Detailed.sol";
 
 contract ProjectToken is ERC1594, ERC20Detailed {
-    //added ERC1594 for token transfer restrictions
     constructor(address treasury) ERC20Detailed("Project Token", "PT", 18) {
         _mint(treasury, maxSupply());
     }
