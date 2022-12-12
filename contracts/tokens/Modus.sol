853e1b563f92d4cbda45aa23802589891d374bfd
Shahill22 <shahill35.ms@gmail.com>
2022-12-12 16:43:45 +0300

    implementation of MODUS ERC20 token

--- contracts/tokens/Modus.sol
+++ contracts/tokens/Modus.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Modus is ERC20 {
-    //implementation of MODUS ERC20 token
     constructor(
         address treasury,
         address founder1,
