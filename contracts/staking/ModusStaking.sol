69ddc2ac0665de921277780d873b2762b0bfd739
Shahill22 <shahill35.ms@gmail.com>
2022-12-23 23:00:32 +0530

    added test cases for basic functions & deposit for staking

--- contracts/staking/ModusStaking.sol
+++ contracts/staking/ModusStaking.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
-contract StakingContract {
+contract ModusStakingContract {
     /* Contract state variables */
     IERC20 public token;
     address public owner;
