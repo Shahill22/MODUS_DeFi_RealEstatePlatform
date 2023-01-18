025743587f760523406cbda7de23be539ffe36a0
Shahill22 <shahill35.ms@gmail.com>
2023-01-18 15:17:04 +0530

    updated kyc contract and modus contract

--- contracts/admin/ModusRegistry.sol
+++ contracts/admin/ModusRegistry.sol
@@ -1,37 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/access/Ownable.sol";
-/*
+
 contract ModusRegistry is Ownable {
-    constructor() Ownable() {}
+    address modusContract;
 
-    mapping(address => bool) public isVerified;
-    mapping(address => mapping(address => bool)) public approvals;
-
-    // Verifies that an address is a real estate property owner
-    function verify(address _address) public {
-        require(msg.sender == owner, "Only the owner can verify addresses.");
-        isVerified[_address] = true;
-    }
-
-    // Transfers ownership of a property
-    function transfer(address _from, address _to, uint256 _tokenId) public {
-        require(isVerified[_from], "Sender must be verified.");
-        require(isVerified[_to], "Receiver must be verified.");
+    constructor(address _modusContractAddress) Ownable() {
         require(
-            approvals[_from][msg.sender],
-            "Only approved addresses can transfer properties."
+            _modusContractAddress != address(0),
+            "ModusRegistry: Invalid token address"
         );
-        require(
-            ownerOf(_tokenId) == _from,
-            "Token ID does not belong to sender."
-        );
-        transferFrom(_from, _to, _tokenId);
-    }
 
-    // Approves a third party to transfer a property on behalf of the owner
-    function approve(address _from, address _to) public {
-        require(isVerified[_from], "Sender must be verified.");
-        approvals[_from][_to] = true;
+        modusContract = _modusContractAddress;
     }
-}*/
+}
